c72bd094231024c32c9e3ef9d0229c887fd96402
Myrrh <92357003+0htoAi@users.noreply.github.com>
2023-09-09 15:44:47 +0800

    Create default_source.cpp

---
+++ problem/code/default_source.cpp
@@ -0,0 +1,17 @@
+
+/*
+stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* i and j are misplaced
+	* variable misuse
+	* do smth instead of nothing and stay organized
+	* vector cause MLE or TLE
+	* wrong file name
+	* DON'T SLEEP IN THE EXAM
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+	* DON'T GET A SWELLED HEAD
+	* YOU MUSTN'T HAVE EMOTION
+->be based on Benq's code
+*/
